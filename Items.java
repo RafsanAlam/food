fb89f828d33dfbabde99466ab4c743c26fee7782
Rafsan Bin Alam <138710619+RafsanAlam@users.noreply.github.com>
2023-07-07 22:39:14 +0600

    Add files via upload

---
+++ Items.java
@@ -0,0 +1,4 @@
+package Hungry;
+
+public class Items {
+}
---
+++ ItemsController.java
@@ -0,0 +1,28 @@
+package Hungry;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class ItemsController {
+
+    @FXML
+    void goToBurger(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToDrink(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToPizza(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToSide(MouseEvent event) {
+
+    }
+
+}
